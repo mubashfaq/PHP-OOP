a7f20d19a0c6c589a65360a2f1c055472da31b82
Mubashar Gtech <mubashir@g-techsolutions.com.au>
2025-12-17 16:19:50 +0500

    06 - ABSTRACTION DONE

---
+++ 06(Abstract).php
@@ -0,0 +1,31 @@
+<?php
+
+// Abstract class cannot use directly (required derived class)
+
+// FOR ABSTRACT CLASS 2 CONNDITION FOLLOWS
+// 1.must have ABSTRACT METHOD (atleast 1)
+// 2.ABSTRACT METHOD only declare not implemented(code)
+// 2.implemented in derived class
+
+abstract class Main {
+    protected $name;
+
+    function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+    abstract protected function show(); // only declare not implemented
+}
+
+class Sub extends Main {
+
+    function show(){
+        echo " Name : " . $this->name;
+    }
+}
+
+$person1 = new Sub('Ali');
+
+$person1->show();
+
