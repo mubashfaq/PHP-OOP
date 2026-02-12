f9b65e0c0c2b067455cc1f797a09d534b6b08c6f
Mubashar Ashfaq <mubashfaq10@gmail.com>
2026-02-12 19:19:35 +0500

    29 - Magic Constants

---
+++ 29(MagicConstant).php
@@ -0,0 +1,12 @@
+<?php
+
+// magic constant have some value at run at specifice place
+// first 4 run anywhere, next 4 required to be in OOP
+// __LINE__ return line number
+// __FILE__ return absolute path of file if pass in file
+// __DIR__ return absolute path of main folder of file if pass in file
+// __FUNCTION__ return name of function if pass in function
+// __CLASS__ return name of class if pass in class
+// __METHOD__ return name of method if  pass in method
+// __NAMESPACE__ return name of namespce 
+// __TRAIT__ return name of trait if pass in trait
