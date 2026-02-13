9e358e68411c4e1114284a3705cbc73cb365160f
Mubashar Ashfaq <mubashfaq10@gmail.com>
2026-02-13 21:10:14 +0500

    30 - Conditional Functions

---
+++ 30(ConditionalFunction).php
@@ -0,0 +1,3 @@
+<?php
+
+// class_exists(), method_exists(), property_exists(), interface_exists(), trait_exists(), is_a() -> check whether object belong to that class, is_subclass_of() -> check  object belong to subclass of parent class
